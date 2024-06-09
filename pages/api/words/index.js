7803a3cbaa530c2dbefa2f518332f9d1585095e0
Best Codes <106822363+The-Best-Codes@users.noreply.github.com>
2024-06-09 16:43:27 +0000

    Umph

--- pages/api/words/index.js
+++ pages/api/words/index.js
@@ -14,15 +14,19 @@ export default async function handler(req, res) {
     return;
   }
 
+  const path = req.query.path || "wordList.json";
+
   try {
     // Read the word list from the JSON file
-    const data = await readFile("./assets/words/wordList.json", "utf8");
+    const data = await readFile(path, "utf8");
     const words = JSON.parse(data);
 
     // Send the array of words as a response
     res.status(200).json(words);
   } catch (error) {
     // Handle any errors that occur during file reading
-    res.status(500).json({ error: "Unable to read the word list." });
+    res
+      .status(500)
+      .json({ error: "Unable to read the word list.", message: error.message });
   }
 }
