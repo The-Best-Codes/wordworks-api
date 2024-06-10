c54f6a1777c14256c8c6470ef926772f4940c0b9
Best Codes <106822363+The-Best-Codes@users.noreply.github.com>
2024-06-10 02:31:52 +0000

    maybe

--- pages/docs/index.js
+++ pages/docs/index.js
@@ -1,4 +1,8 @@
 // pages/docs/index.js
+
+import swaggerJsdoc from "swagger-jsdoc";
+import React from "react";
+
 export async function getStaticProps(context) {
   const swaggerSpecification = swaggerJsdoc({
     // Your configuration here
@@ -8,3 +12,7 @@ export async function getStaticProps(context) {
     props: { swaggerSpecification }, // will be passed to the page component as props
   };
 }
+
+export default function Docs({ swaggerSpecification }) {
+    return <div>{swaggerSpecification}</div>;
+  }
