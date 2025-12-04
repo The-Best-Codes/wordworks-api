250bb2e55009a53b11b7bcbacb3c3262d570701f
The-Best-Codes <bestcodes.official@gmail.com>
2025-12-03 19:39:39 -0600

    chore: update deps

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  experimental: {
-    turbo: {
-      treeShaking: true,
-    },
-  },
 };
 
 export default nextConfig;
